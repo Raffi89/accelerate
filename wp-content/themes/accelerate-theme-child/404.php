17ab16c72a39c4129c3590ce0ddff4c6df1557d7
raffi <raffi.matcha@gmail.com>
2018-05-28 16:56:02 +0200

    a random message to fix the styles

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+  <link rel="stylesheet" href="styles.css">
+	<div id="primary" class="site-content">
+		<div class="main-content" role="main">
+		<img src="broken-cat.jpg" alt="broken cat">
+				<h2>404 Cat .exe has stopped working.</h2>
+        <p>It looks like you broke teh interwebs.</p>
+        <p>Great job!</p>
+				<?php the_content(); ?>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
