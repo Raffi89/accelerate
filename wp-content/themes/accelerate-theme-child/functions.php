1f91f6f774b53a946c42299bda0a9908f4b665e2
raffi <raffi.matcha@gmail.com>
2018-06-05 21:39:55 +0200

    new about page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -34,5 +34,20 @@ function create_custom_post_types() {
 			)
 	);
 }
-// hook this custom post type function into the theme
+// hook this about page function into the theme
 add_action( 'init', 'create_custom_post_types' );
+
+function create_about_page() {
+    register_post_type( 'about_page',
+        array(
+            'labels' => array(
+                'name' => __( 'About Us' ),
+                'singular_name' => __( 'About Us' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'about-us' ),
+        )
+    );
+}
+add_action( 'init', 'create_about_page' );
